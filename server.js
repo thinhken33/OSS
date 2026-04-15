e90f4d384da63ec72de0cbce7ccbc001fd9d03ed
HO VAN THINH <1250080185@sv.hcmunre.edu.vn>
2026-04-15 09:57:15 +0700

    test routes

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const path = require("path");
-const taskRoutes = require("./routes/taskRoutes");
+const taskRoutes = require("./routes/taskRoutes.js");
 
 const app = express();
 const PORT = 3000;
